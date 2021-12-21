381e60779adfb731dbffe30e04d9ea4f3e9ff594
lsw110555 <lsw110555@gmail.com>
2021-12-21 23:04:29 +0900

    퍼블리싱 1일차 2

---
+++ 1일차/script/18.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("p").css({"display": "none"})
+
+  $(".show-btn").click(function() {
+    $("p").css({
+      "display" : "block"
+    })
+  })
+  $(".hide_btn").click(function() {
+    $("p").css({
+      "display" : "none"
+    })
+  })
+
+
+
+
+
+
+})
